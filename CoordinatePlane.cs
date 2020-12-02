aa79cf8c6e3292db849ae9fc9845e74c84dfdc6b
hyunjin.gil <hyunjin.direct@gmail.com>
2020-12-02 13:21:30 +0900

    초기 커밋.

---
+++ CoordinatePlane.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+//using System.Drawing; 
+
+namespace CircleApp
+{
+    static class test
+    {
+        public static double ToRadians(this int val) //확장메서드
+        {
+            return (0.017453292519943295 * val); //1라디안 * 각도
+        }
+    }
+    
+    struct Point
+    {
+        private readonly double _x; //초기화시만 수정가능한(readonly) 가능한 변수
+        private readonly double _y;
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override string ToString()
+        {
+            return "X : " + _x + " Y : " + _y;
+        }
+    } //double PointStruct
+    class CoordinatePlane
+    {
+        const int MAX_RADIAN = 360; // 원의 최대 각도
+        List<Point> ptList = new List<Point>(); //원 위의 점 좌표를 관리할 리스트 컬렉션
+
+        public List<Point> PtList {get { return ptList; }} //외부에서 리스트 컬렉션에 읽기 만 가능하도록.
+
+        public bool MakeCircle(int radian, int cnt, int cx = 0, int cy =0)//원점 x,y는 디폴트로 0,0
+        {
+            ptList.Clear();// 이미 그린 원이 있을 수 있으므로 컬렉션 클리어
+            double angle = MAX_RADIAN / cnt; // 원의 최대 각도에서 등분을 나누어서 각도를 구함.
+
+            try
+            {
+                for (int i = 0; i < MAX_RADIAN; i += (int)angle) //i = 0 부터 360도까지 각도 나눠서 더함
+                {
+
+                    double x = Math.Round((double)(Math.Cos(i.ToRadians()) * radian), 3) - cx;
+                    double y = Math.Round((double)(Math.Sin(i.ToRadians()) * radian), 3) - cy;
+                    //반올림 소수 셋째자리까지
+                    //반지름 * Cos - 원점X = 원점으로부터 원 위의 x좌표 
+                    //반지름 * Sin - 원점 Y = 원 위의 y좌표
+                    ptList.Add(new Point(x, y));
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.GetBaseException());
+                return false; //실패
+            }
+            return true; //원을 제대로 만듬
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static int Menu()
+        {
+            Console.Clear(); // 메뉴가 불러질 때마다 화면 청소
+            Console.WriteLine("[0] 프로그램 종료 [1] 원 그리기 [2] 그린 원 출력");
+
+            try
+            {
+                return int.Parse(Console.ReadLine()); //메뉴 입력받아서 리턴
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.GetBaseException()); //에러 출력
+                return -1;
+            }
+        }
+
+        static void InputOriginCircleInfo(CoordinatePlane cp)
+        {
+            int radius = 0 , cnt = 0 , cx = 0 , cy = 0;
+            // 반지름, 등분, 원점 X, 원점 Y
+            try  
+            {
+                Console.Write("그릴 원의 반지름을 입력하세요 : ");
+                radius = int.Parse(Console.ReadLine());
+
+                Console.Write("원을 몇 등분 하시겠습니까? : ");
+                cnt = int.Parse(Console.ReadLine());
+
+                Console.Write("원점 X 좌표 값 입력 : ");
+                cx = int.Parse(Console.ReadLine());
+                
+                Console.Write("원점 Y 좌표 값 입력 : ");
+                cy = int.Parse(Console.ReadLine());
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.GetBaseException());
+                return;
+            }
+
+            cp.MakeCircle(radius, cnt,cx,cy);// 좌표평면에 원을 만들때 필요한 원점과 반지름, 등분수를 보냄
+        }
+
+       
+        static void PrintPoint(CoordinatePlane cp)
+        {
+            var subset = from i in cp.PtList select i; 
+            // ptList로부터 LINQ를 통해 원 위의 임의의 등분한 점을 출력
+            foreach (var s in subset)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CoordinatePlane cp = new CoordinatePlane(); //좌표평면
+            int key = 0; // 메뉴 입력 변수
+    
+            for(;;)
+            if ((key = Menu()) != -1)
+            {
+                switch (key)
+                {
+                    case 1: InputOriginCircleInfo(cp); break; //원점에 대한 원을 그리고 저장된 원의 좌표를 출력
+                    case 2: PrintPoint(cp); break;  //좌표 평면 원 위의 등분한 임의의 점을 출력
+                    case 0: return ; //종료
+                }
+                Console.WriteLine("아무키나 누르세요");
+                Console.ReadLine(); //무한루프 대기 입력
+            }
+        }
+    }
+}
